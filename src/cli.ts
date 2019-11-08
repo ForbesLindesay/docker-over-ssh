868bd15f1b8d13733fe2b29bd69c1da89dd50ad9
Forbes Lindesay <forbes@lindesay.co.uk>
2019-11-08 02:37:47 +0000

    fix: add help text to CLI

--- src/cli.ts
+++ src/cli.ts
@@ -6,6 +6,14 @@ import progress from 'progress-stream';
 import logUpdate from 'log-update';
 import bytes from 'bytes';
 
+if (process.argv.includes('--help')) {
+  help();
+  process.exit(0);
+}
+if (!process.argv[3]) {
+  help();
+  process.exit(1);
+}
 switch (process.argv[2]) {
   case 'pull':
     pull(process.argv[3], process.stdin, process.stdout).then(
@@ -19,6 +27,10 @@ switch (process.argv[2]) {
     );
     break;
   case 'push':
+    if (!process.argv[4]) {
+      help();
+      process.exit(1);
+    }
     const child = process.argv.slice(4);
     push(process.argv[3], async (stream) => {
       const prog = progress({time: 100}, (e) => {
@@ -41,4 +53,19 @@ switch (process.argv[2]) {
       process.exit(1);
     });
     break;
+  default:
+    help();
+    process.exit(1);
+    break;
+}
+
+function help() {
+  console.info(
+    'To transfer an image called `node:12-alpine` to a server called `dokku` over `ssh`, run:',
+  );
+  console.info();
+  console.info(
+    '  docker-over-ssh push node:12-alpine ssh dokku "docker-over-ssh pull node:12-alpine"',
+  );
+  console.info();
 }
