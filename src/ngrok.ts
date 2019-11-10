e2b3973c9419726d8dca448f69b8d7f1ddb20867
Forbes Lindesay <forbes@lindesay.co.uk>
2019-11-10 14:40:30 +0000

    feat: password protected ngrok

--- src/ngrok.ts
+++ src/ngrok.ts
@@ -1,54 +1,57 @@
 import {URL} from 'url';
+import generatePassCode, {Encoding} from '@authentication/generate-passcode';
+import {run} from '@databases/with-container';
 
+export interface NGrokOptions {
+  /**
+   * http also supports https
+   *
+   * default: http
+   */
+  proto?: 'http' | 'tcp' | 'tls';
+  /**
+   * default: 80
+   */
+  addr?: number; // port or network address, defaults to 80
+  /**
+   * http basic authentication for tunnel
+   *
+   * format: user:pwd
+   */
+  auth?: string;
+  /**
+   * reserved tunnel name e.g. 'alex' =>  'https://alex.ngrok.io'
+   */
+  subdomain?: string;
+  /**
+   * your authtoken from ngrok.com
+   */
+  authtoken?: string;
+  /**
+   * one of ngrok regions (us, eu, au, ap)
+   * default: us
+   */
+  region?: 'us' | 'eu' | 'au' | 'ap';
+  /**
+   * custom path for ngrok config file
+   */
+  configPath?: string;
+  /**
+   * custom binary path, eg for prod in electron
+   */
+  binPath?: (path: string) => string;
+  /**
+   * 'closed' - connection is lost, 'connected' - reconnected
+   */
+  onStatusChange?: (status: 'closed' | 'connected') => void;
+  /**
+   * returns stdout messages from ngrok process
+   */
+  onLogEvent?: (data: Buffer) => void;
+}
 export interface NGrok {
   authtoken(token: string): Promise<void>;
-  connect(options: {
-    /**
-     * http also supports https
-     *
-     * default: http
-     */
-    proto?: 'http' | 'tcp' | 'tls';
-    /**
-     * default: 80
-     */
-    addr?: number; // port or network address, defaults to 80
-    /**
-     * http basic authentication for tunnel
-     *
-     * format: user:pwd
-     */
-    auth?: string;
-    /**
-     * reserved tunnel name e.g. 'alex' =>  'https://alex.ngrok.io'
-     */
-    subdomain?: string;
-    /**
-     * your authtoken from ngrok.com
-     */
-    authtoken?: string;
-    /**
-     * one of ngrok regions (us, eu, au, ap)
-     * default: us
-     */
-    region?: 'us' | 'eu' | 'au' | 'ap';
-    /**
-     * custom path for ngrok config file
-     */
-    configPath?: string;
-    /**
-     * custom binary path, eg for prod in electron
-     */
-    binPath?: (path: string) => string;
-    /**
-     * 'closed' - connection is lost, 'connected' - reconnected
-     */
-    onStatusChange?: (status: 'closed' | 'connected') => void;
-    /**
-     * returns stdout messages from ngrok process
-     */
-    onLogEvent?: (data: Buffer) => void;
-  }): Promise<string>;
+  connect(options: NGrokOptions): Promise<string>;
   disconnect(url?: string): Promise<void>;
   kill(): Promise<void>;
 }
@@ -64,10 +67,10 @@ let runningNGroks = 0;
 export default async function getHost<T>(
   port: number,
   defaultHost: string,
-  run: (host: string) => Promise<T>,
+  withHost: (host: string) => Promise<T>,
 ): Promise<T> {
   if (!process.env.DOCKER_REGISTRY_NGROK) {
-    return await run(`${defaultHost}:${port}`);
+    return await withHost(`${defaultHost}:${port}`);
   }
   if (!ngrok) {
     throw new Error(
@@ -75,17 +78,36 @@ export default async function getHost<T>(
     );
   }
 
-  const urlString = await ngrok.connect({
-    addr: port,
-    authtoken:
-      process.env.DOCKER_REGISTRY_NGROK === 'true'
-        ? undefined
-        : process.env.DOCKER_REGISTRY_NGROK,
-  });
+  const auth =
+    process.env.DOCKER_REGISTRY_NGROK === 'true'
+      ? null
+      : await Promise.all([
+          generatePassCode(10, Encoding.base32),
+          generatePassCode(60, Encoding.base32),
+        ]).then(([user, pass]) => ({user, pass}));
+  const options: NGrokOptions =
+    process.env.DOCKER_REGISTRY_NGROK === 'true'
+      ? {addr: port}
+      : {
+          addr: port,
+          authtoken: process.env.DOCKER_REGISTRY_NGROK,
+          auth: auth ? `${auth.user}:${auth.pass}` : undefined,
+        };
+  const urlString = await ngrok.connect(options);
   runningNGroks++;
   try {
     const url = new URL(urlString);
-    return await run(url.host);
+    if (auth) {
+      await run(
+        'docker',
+        ['login', '-u', auth.user, '-p', auth.pass, url.host],
+        {
+          debug: false,
+          name: 'docker login',
+        },
+      );
+    }
+    return await withHost(url.host);
   } finally {
     await ngrok.disconnect(urlString);
     setTimeout(async () => {
